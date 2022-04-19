7418e3b41fb5a22a05a6896ef3f0f8ebf6ea7579
Quantata <ceoahn95@gmail.com>
2022-04-19 21:55:07 +0900

    DataType, Function, RangeClass 관련 함수 문법

---
+++ kotlinlearning/src/main/java/com/quantata/kotlinlearning/BasicDataType.kt
@@ -0,0 +1,23 @@
+package com.quantata.kotlinlearning
+
+class BasicDataType {
+
+    companion object {
+        fun result() {
+            /** 기본 자료형 **/
+            // 배열 자료형
+            val stringArray : Array<String> = arrayOf("apple", "banana", "grape")
+            val intArray = arrayOf(1,2,3) // 자료형 생략
+
+            println(stringArray[0])
+            println(intArray[2])
+
+            // 명시적 형변환
+            val score = 100 // Int형
+            val scoreString = score.toString() // String 형
+            val scoreDouble = score.toDouble() // Double 형
+
+            println(scoreDouble) // 100.0
+        }
+    }
+}
---
+++ kotlinlearning/src/main/java/com/quantata/kotlinlearning/BasicFunction.kt
@@ -0,0 +1,19 @@
+package com.quantata.kotlinlearning
+
+class BasicFunction {
+    companion object {
+        fun result() {
+            // 반환값이 있는 함수
+            fun addNum(a: Int, b: Int) : Int {
+                return a + b
+            }
+
+            println(addNum(2,3))
+
+            // 단일 표현식 함수 = 실행할 코드가 표현식 하나로 이루어진 함수
+            fun minusNum(a: Int, b: Int) = a - b
+
+            println(minusNum(minusNum(100, 30), 10))
+        }
+    }
+}
