85e6804a7c00fd1c8c8626187e2ecabedacaaa10
Quantata <ceoahn95@gmail.com>
2022-04-22 09:52:10 +0900

    StopWatch data binding 기능 추가

--- stopwatch/src/main/java/com/quantata/stopwatch/MainActivity.kt
+++ stopwatch/src/main/java/com/quantata/stopwatch/MainActivity.kt
@@ -2,10 +2,23 @@ package com.quantata.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.quantata.stopwatch.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener { // 클릭 이벤트 처리 인터페이스 = OnClickListener
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.main = this
+
     }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+
+
 }
